60421f3b8a37b87b8d940e93a08f74bacc19a8b7
THMKruemel <lennartsigge@icloud.com>
2025-11-20 18:44:22 +0100

    Fixed deploy file

--- svelte.config.js
+++ svelte.config.js
@@ -2,11 +2,17 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+const base = process.env.PUBLIC_BASE_PATH || '';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+        adapter: adapter(),
+        paths: {
+            base
+        }
+    }
 };
 
 export default config;
